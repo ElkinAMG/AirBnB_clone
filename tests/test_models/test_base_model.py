ea0fa79630c152c62e3629824b7b7774ff4e658c
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-30 10:20:22 -0500

    Test BaseModel Started

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,15 @@
+"""
+Unittesting for `BaseModel`.
+"""
+import unittest
+from models import BaseModel
+
+class BaseModelTest(unittest.TestCase):
+	"""
+	Testing Class for `BaseModel`
+	"""
+
+	def setUp(self):
+		self.BaseModel = BaseModel()
+	
+	def 
