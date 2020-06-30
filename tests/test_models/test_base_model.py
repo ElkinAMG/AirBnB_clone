529dd748880cd39d217fff54e5e4bb33cd115fdc
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-30 16:43:47 -0500

    Switched Previuos Commits

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,34 +13,56 @@ class BaseModelTest(unittest.TestCase):
 
     def setUp(self):
         """set up a base class model"""
-        self.base = BaseModel()
+        self.ins = BaseModel()
 
-    def test_checking_for_docstring_BaseModel(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(BaseModel.__doc__)
-        self.assertIsNotNone(BaseModel.__init__.__doc__)
-        self.assertIsNotNone(BaseModel.__str__.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-        self.assertIsNotNone(BaseModel.to_dict.__doc__)
-
-    def test_method_BaseModel(self):
-        """chekcing if Basemodel have methods"""
+    def test_hasattr(self):
+        """check if base model has the required attributes"""
         self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "__str__"))
         self.assertTrue(hasattr(BaseModel, "save"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
 
-    def test_init_BaseModel(self):
-        """test if the base is an type BaseModel"""
-        self.assertTrue(isinstance(self.base, BaseModel))
+    def test_doc_string(self):
+        """testing if docstrings exist"""
+        self.assertEqual(len(BaseModel.__doc__) > 0, True)
+        self.assertEqual(len(BaseModel.__init__.__doc__) > 0, True)
+        self.assertEqual(len(BaseModel.__str__.__doc__) > 0, True)
+        self.assertEqual(len(BaseModel.save.__doc__) > 0, True)
+        self.assertEqual(len(BaseModel.to_dict.__doc__) > 0, True)
 
-    def test_save_BaesModel(self):
-        """test if the save works"""
-        self.base.save()
-        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+    def test_instance(self):
+        """testing to see whether created instance is equal to BaseModel"""
+        self.assertTrue(isinstance(self.ins, BaseModel))
+        self.assertTrue(self.ins.__class__ == BaseModel)
 
-    def test_to_dict_BaseModel(self):
-        """test if dictionary works"""
-        base_dict = self.base.to_dict()
-        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
-        self.assertIsInstance(base_dict['created_at'], str)
-        self.assertIsInstance(base_dict['updated_at'], str)
+    def test_save_BaseModel(self):
+        """testig whether save methd works for BaseModel"""
+        self.ins.save()
+        self.assertNotEqual(self.ins.created_at, self.ins.updated_at)
+        with open("file.json", "r") as f:
+            lines = f.read()
+            self.assertTrue(len(lines) > 0)
+
+    def test_to_dict(self):
+        """test to dict method of base model"""
+        dic = self.ins.to_dict()
+        self.assertTrue(len(dic) > 0)
+        self.assertTrue(isinstance(dic, dict))
+        self.assertTrue(isinstance(dic['created_at'], str))
+        self.assertTrue(isinstance(dic['updated_at'], str))
+        self.assertTrue(isinstance(dic['id'], str))
+
+    def test_str_method(self):
+        """test whether the string method returns the correct output"""
+
+        id = self.ins.id
+        cln = self.ins.__class__.__name__
+        dic = self.ins.__dict__
+        string = self.ins.__str__()
+        self.assertEqual(string, "[{}] ({}) {}".format(cln, id, dic))
+
+    def test_update(self):
+        """test whether the update method updates an object properly"""
+        new = BaseModel(**(self.ins.to_dict()))
+        for k, v in new.to_dict().items():
+            self.assertEqual(new.to_dict()[k], self.ins.to_dict()[k])
